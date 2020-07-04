8f1239f28f0df6ab8ad2b554129a01a566a565ec
nickdpz <ndpastranzamora@gmail.com>
2020-07-04 00:48:48 -0500

    RTC

--- Wifi/NodoWifi/NodoWifi.ino
+++ Wifi/NodoWifi/NodoWifi.ino
@@ -1,7 +1,11 @@
+#include "Arduino.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <NewPing.h>
-#include <Ticker.h>
+// Required for LIGHT_SLEEP_T delay mode
+extern "C" {
+#include "user_interface.h"
+}
 
 #define LED_STATUS     0//0 - D3
 #define LED_WIFI       2//2 - D4
@@ -9,45 +13,40 @@
 #define ECHO_PIN_1     13//13 - D7
 #define ECHO_PIN_2     12//12 - D6
 #define BUTTON_C       5//16 - D0//5 - D1
-#define MAX_DISTANCE    500
+#define MAX_DISTANCE   500
 
 #define SERVER_IP_ALERT "http://192.168.0.7:3000/routes"
 #define SERVER_IP_TEST "http://192.168.0.7:3000/routes/test"
 #define SERVER_IP_SUCCESS "http://192.168.0.7:3000/routes/success"
 
+NewPing sonar1(TRIGGER_PIN, ECHO_PIN_1, MAX_DISTANCE);
+const char* stassid = "JAPEREZ";
+const char* stapsk = "26071967";
+
 const int timeThreshold = 150;
 long startTime = 0;
-
-NewPing sonar1(TRIGGER_PIN, ECHO_PIN_1, MAX_DISTANCE);
-char stassid[] = "JAPEREZ";
-char stapsk[] = "26071967";
 char buffer[60] = "";
 int distance = 0;
+int bootCount = 0;
 bool flagCollect = false;
 bool flagSuccess = false;
 bool flagAlert1 = false;
-bool flagAlert2 = false;
 bool flagInterrupt = true;
+int count = 0;
 WiFiClient client;
 HTTPClient http;
-Ticker ticker;
 
 void alert() {
-  sprintf(buffer, "{\"eui\":\"%s\",\"password\":\"%s\",\"data\":%d}", stassid, stapsk, distance);
+  sprintf(buffer, "{\"eui\":\"%s\",\"psk\":\"%s\",\"data\":%d}", stassid, stapsk, distance);
   Serial.print("[HTTP] begin...\n");
   // configure traged server and url
   http.begin(client, SERVER_IP_TEST); //HTTP
   http.addHeader("Content-Type", "application/json");
-
   int httpCode = http.POST(buffer);
   //int httpCode = http.POST("{\"hello\":\"world\"}");
   String payload = http.getString();
-  // httpCode will be negative on error
   if (httpCode > 0) {
-    // HTTP header has been send and Server response header has been handled
     Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-
-    // file found at server
     if (httpCode == HTTP_CODE_OK) {
       const String& payload = http.getString();
       Serial.println("received payload:\n<<");
@@ -61,20 +60,14 @@ void alert() {
 }
 
 void success() {
-  sprintf(buffer, "{\"eui\":\"%s\",\"password\":\"%s\"}", stassid, stapsk);
+  sprintf(buffer, "{\"eui\":\"%s\",\"psk\":\"%s\"}", stassid, stapsk);
   Serial.print("[HTTP] begin...\n");
-  // configure traged server and url
   http.begin(client, SERVER_IP_SUCCESS); //HTTP
   http.addHeader("Content-Type", "application/json");
-
   int httpCode = http.POST(buffer);
   String payload = http.getString();
-  // httpCode will be negative on error
   if (httpCode > 0) {
-    // HTTP header has been send and Server response header has been handled
     Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-
-    // file found at server
     if (httpCode == HTTP_CODE_OK) {
       const String& payload = http.getString();
       Serial.println("received payload:\n<<");
@@ -98,7 +91,9 @@ void IRAM_ATTR irq_button_c()
       if (flagCollect) {
         flagSuccess = true;
         flagAlert1 = false;
-        flagAlert2 = false;
+         Serial.println("Close Container");
+      } else {
+        Serial.println("Open Container");
       }
       flagCollect = !flagCollect;
       startTime = millis();
@@ -106,56 +101,75 @@ void IRAM_ATTR irq_button_c()
   }
 }
 
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_WIFI, OUTPUT);
-  pinMode(LED_STATUS, OUTPUT);
-  pinMode(BUTTON_C, INPUT_PULLUP);
+void initWiFi() {
   attachInterrupt(digitalPinToInterrupt(BUTTON_C), irq_button_c, FALLING);
-  Serial.begin(9600);
-
-
+  WiFi.mode(WIFI_STA);
   WiFi.begin(stassid, stapsk);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
-  digitalWrite(LED_WIFI, HIGH);
+  Serial.println("Connected!");
+  digitalWrite(LED_STATUS, flagAlert1);
 }
-
-// the loop function runs over and over again forever
+//The setup function is called once at startup of the sketch
+void setup() {
+  pinMode(LED_WIFI, OUTPUT);
+  pinMode(LED_STATUS, OUTPUT);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+  digitalWrite(LED_WIFI, LOW);
+  digitalWrite(LED_STATUS, LOW);
+  Serial.begin(9600);
+  Serial.println("Finish Config");
+}
+void callback() {
+  Serial.println("Callback");
+  Serial.flush();
+}
 void loop() {
-  if ((WiFi.status() == WL_CONNECTED) && (!flagCollect)) {
-    statusOn();
+  Serial.println("Enter light sleep mode");
+  detachInterrupt(digitalPinToInterrupt(BUTTON_C));
+  //uint32_t sleep_time_in_s = 100000;
+  uint32_t sleep_time_in_s = 3600;//1 minite
+  wifi_set_opmode(NULL_MODE);
+  gpio_pin_wakeup_enable(GPIO_ID_PIN(BUTTON_C), GPIO_PIN_INTR_HILEVEL);
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+  wifi_fpm_open();
+  wifi_fpm_set_wakeup_cb(callback);
+  wifi_fpm_do_sleep(sleep_time_in_s *1000 * 1000 );
+  delay(sleep_time_in_s + 1);
+  Serial.println("Exit light sleep mode");
+  initWiFi();
+  if (!flagCollect) {
     distance = sonar1.ping_cm();
-    if (distance == 0) {
-      distance = 200;
-    }
-    Serial.print("Ping: ");
-    Serial.print(distance);
-    Serial.println("cm");
-    if (flagSuccess) {
-      if (distance > 40) {
-        success();
+    for (uint8_t count = 0; count < 30; count++) {
+      if (WiFi.status() == WL_CONNECTED) {
+        distance = (sonar1.ping_cm() + distance) / 2;
+        if (distance == 0) {
+          distance = 200;
+        }
+        Serial.print("Ping: ");
+        Serial.print(distance);
+        Serial.println("cm");
+        if (flagSuccess) {
+          if (distance > 35) {
+            success();
+          }
+          flagSuccess = false;
+        }
+        if (distance < 37) {
+          flagAlert1 = true;
+          digitalWrite(LED_STATUS, flagAlert1);
+        }
+        statusOn();
       }
-      flagSuccess = false;
     }
-    if ((distance < 15) && (flagAlert1)) {
-      alert();
-      flagAlert2 = true;
-    }
-    if ((distance < 37) && (!flagAlert1)&&(!flagAlert2)) {
-      alert();
-      digitalWrite(LED_STATUS, HIGH);
-      flagAlert1 = true;
-    }
-
-
   }
+  alert();
+  wifi_set_sleep_type(NONE_SLEEP_T);
+  delay(10000);  //  Put the esp to sleep for 15s
 }
+
 void statusOn() {
   digitalWrite(LED_WIFI, HIGH);
   delay(1000);
